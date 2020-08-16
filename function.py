6b16e1e0edaef7777b685e389d60af7619b362bd
Chandra-mouli-kante <57706070+Chandra-mouli-kante@users.noreply.github.com>
2020-08-16 22:13:59 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,50 @@
+'''
+def add():
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a+b)
+add()
+add()
+'''
+
+'''
+print("Hi")
+def add():
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a+b)
+print("hello")
+add()
+print("example")
+add()
+'''
+
+'''
+def add():
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    return(a+b)
+print(add())
+'''
+
+'''
+def add():
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    return(2)
+print(add())
+'''
+
+'''
+def add(a,b):
+    return(a+b)
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+print(add(a,b))
+'''
+
+
+def add(a,b):
+    return(a+b)
+print(add(2,3))
+print(add(5,3))
