3ad39dde03f829847527b20878a29950f68952b4
Chandra-mouli-kante <57706070+Chandra-mouli-kante@users.noreply.github.com>
2020-08-20 12:29:58 +0530

    Add files via upload

---
+++ function_usecases.py
@@ -0,0 +1,27 @@
+'''
+import math
+a=math.ceil(23.0212)
+b=math.ceil(-23.0212)
+print(a,b)
+'''
+
+'''
+import math
+a=math.floor(23.0212)
+b=math.floor(-23.0212)
+print(a,b)
+'''
+
+'''
+from math import ceil
+a=ceil(23.0212)
+b=ceil(-23.0212)
+print(a,b)
+'''
+
+
+a='hi this is module'
+print(a)
+n=int(input("enter a number"))
+import Programs
+print(Programs.sum_digits(n))
