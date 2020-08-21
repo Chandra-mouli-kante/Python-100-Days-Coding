c37c83540686b844a5fee45e133f0affa8e2ca71
Chandra-mouli-kante <57706070+Chandra-mouli-kante@users.noreply.github.com>
2020-08-21 18:08:35 +0530

    Add files via upload

---
+++ patterns2.py
@@ -0,0 +1,44 @@
+'''
+for i in range(1,6):
+    for j in range(1,6):
+        print(j,end=" ")
+    print()
+'''
+
+'''
+for i in range(5):
+    print("1 2 3 4 5")
+'''
+
+'''
+for i in range(1,6):
+    for j in range(1,6):
+        print(i,end=" ")
+    print()
+'''
+
+'''
+for i in range(1,10):
+    for j in range(1,6):
+        print(j,end=" ")
+    print()
+'''
+
+'''
+n=int(input("Enter a number"))
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end=' ')
+    print()
+'''
+
+'''
+n=int(input("Enter a number"))
+for i in range(1,n+1):
+     print(i*"* ")
+'''
+
+
+n=int(input("Enter a number"))
+for i in range(1,n+1):
+     print(i*"* ",end=" ")
